da333f2218fd8f76ca5f907b92c7595affb64d17
Владимир Цону <123900549+VladimirTsonu@users.noreply.github.com>
2023-07-21 01:45:05 +0500

    Add files via upload

---
+++ Program 01.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int number, int coef)
+{
+    int result = number;
+    for (int i = 1; i < coef; i++)
+    {
+        result = result * number;
+    }
+    Console.WriteLine($"Your result is: {result} ");
+    return result;
+}
+Console.WriteLine("Enter your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your coef: ");
+int my_coef = Convert.ToInt32(Console.ReadLine());
+Exponentiation(user_number, my_coef);
---
+++ Program 02.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int SumDigit(int number)
+
+{
+    int result = 0;
+    int sum = 0;
+    int sumTotal = 0;
+    while (number > 10)
+    {
+        result = number % 10;
+        number = number / 10;
+        sum = sum + result;
+    }
+    sumTotal = sum + number;
+    Console.WriteLine($"Your result is: {sumTotal}");
+    return sumTotal;
+}
+Console.WriteLine("Enter your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+SumDigit(user_number);
